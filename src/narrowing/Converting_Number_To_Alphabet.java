9ce350b91761b176ecbe3f6ffd7bd0d2939205a1
Narayan Thakare <narayan111thakare@gmail.com>
2024-07-18 23:40:59 +0530

    first commit

---
+++ src/narrowing/Converting_Number_To_Alphabet.java
@@ -0,0 +1,23 @@
+package narrowing;
+
+public class Converting_Number_To_Alphabet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int i = 97;
+		
+		char ch = (char)i;
+		
+	System.out.println(ch);
+		
+		
+		
+		
+		
+
+	}
+
+}
